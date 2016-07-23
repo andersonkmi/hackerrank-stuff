beab68120bf811670aaf6e5cf17d75db80a7cbd4
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-07-23 20:49:11 -0300

    one more item developed

--- Hackerrank/src/main/java/org/coolstuff/NewYearChaos.java
+++ Hackerrank/src/main/java/org/coolstuff/NewYearChaos.java
@@ -12,19 +12,46 @@ public class NewYearChaos {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in = new Scanner(System.in);
         int testCases = in.nextInt();
-        List<int[]> inputData = new ArrayList<>();
-        for(int counter = 0; counter < testCases; counter++) {
-            int quantityNumbers = in.nextInt();
-            int[] testCase = new int[quantityNumbers];
-            for(int index = 0; index < quantityNumbers; index++) {
-                testCase[index] = in.nextInt();
-                inputData.add(testCase);
+
+        while(testCases > 0) {
+            testCases--;
+
+            int quantity = in.nextInt();
+            int[] numbers = new int[quantity];
+            for(int index = 0; index < numbers.length; index++) {
+                numbers[index] = in.nextInt();
+            }
+
+            int[] frequency = new int[quantity + 1];
+            boolean isChaos = false;
+            boolean isFinished = false;
+            int counter = 0;
+            while(!isFinished) {
+                isFinished = true;
+
+                for(int index = 0; index < quantity - 1; index++) {
+                    if(numbers[index] > numbers[index + 1]) {
+                        frequency[numbers[index]]++;
+                        if(frequency[numbers[index]] > 2) {
+                            isChaos = true;
+                            isFinished = true;
+                            break;
+                        }
+
+                        counter++;
+                        int temp = numbers[index];
+                        numbers[index] = numbers[index + 1];
+                        numbers[index + 1] = temp;
+                        isFinished = false;
+                    }
+                }
+            }
+
+            if(isChaos) {
+                System.out.println("Too chaotic");
+            } else {
+                System.out.println(counter);
             }
         }
-
-        // Structure where the result will be stored
-        List<String> results = new ArrayList<>();
-
-        // For each item in the list, verifies if there were bribes
     }
 }
