d5b654d5b410279466ea3dd16c3bf3f2f0bda18a
Anderson Keiji Matsuki Ito <anderson.ito@payu.com.br>
2016-07-29 12:47:37 -0300

    More stuff - reviewing coding challenge

---
+++ Hackerrank/src/main/java/org/coolstuff/HotelSorting.java
@@ -0,0 +1,22 @@
+package org.coolstuff;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * Created by anderson.ito on 29/07/2016.
+ */
+public class HotelSorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+
+    private static List<Integer> sortHotels(List<String> keywords,  List<String> reviews) {
+        for(String review : reviews) {
+            StringTokenizer tokenizer = new StringTokenizer(review, ",");
+
+        }
+        return null;
+    }
+}
