085a89070bb984db102f1ef55e392779ab055af8
andersonkmi <andersonkmi@acm.org>
2016-08-02 23:44:17 -0300

    Mode code

---
+++ Hackerrank/src/main/java/org/coolstuff/graph/GraphPath.java
@@ -0,0 +1,7 @@
+package org.coolstuff.graph;
+
+/**
+ * Created by andersonkmi on 8/2/16.
+ */
+public class GraphPath {
+}
---
+++ Hackerrank/src/main/java/org/coolstuff/graph/Status.java
@@ -0,0 +1,8 @@
+package org.coolstuff.graph;
+
+/**
+ * Created by andersonkmi on 8/2/16.
+ */
+public enum Status {
+    Unvisited, Visiting, Visited;
+}
