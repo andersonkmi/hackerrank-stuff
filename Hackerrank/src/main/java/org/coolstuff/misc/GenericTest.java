fa3f2ba0c6794db8914c36a7386bea7774c4fe13
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-08-07 09:54:24 -0300

    More exercises

---
+++ Hackerrank/src/main/java/org/coolstuff/misc/GenericTest.java
@@ -0,0 +1,10 @@
+package org.coolstuff.misc;
+
+/**
+ * Created by andersonkmi on 8/6/2016.
+ */
+public class GenericTest<T extends Comparable> {
+    public void run(T first, T second) {
+        System.out.println(first.compareTo(second));
+    }
+}
