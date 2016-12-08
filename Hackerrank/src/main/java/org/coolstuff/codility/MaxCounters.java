34b43cde9602c14184e575c943f7073af03bd69a
Anderson Ito <anderson.ito@yandeh.com.br>
2016-12-08 13:00:51 -0200

    New exercise

---
+++ Hackerrank/src/main/java/org/coolstuff/codility/MaxCounters.java
@@ -0,0 +1,23 @@
+package org.coolstuff.codility;
+
+/**
+ * Created by anderson.ito on 12/8/2016.
+ */
+public class MaxCounters {
+    public int[] solution(int N, int[] A) {
+        // write your code in Java SE 8
+        int[] counters = new int[N];
+
+        return null;
+    }
+
+    private void increase(int pos, int[] array) {
+        array[pos] = array[pos] + 1;
+    }
+
+    private void maxCounters(int[] array) {
+        for(int index = 0; index < array.length; index++) {
+
+        }
+    }
+}
